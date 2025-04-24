7f09df6b0ae89b14e161813ea14defae188b0853
Alexey <borgleha200023@gmail.com>
2025-04-24 13:07:33 +0700

    fix

--- frontend_DZ/react/number-facts/numbers-facts-app/src/index.js
+++ frontend_DZ/react/number-facts/numbers-facts-app/src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // Опционально, если у вас есть глобальные стили
+import './index.css';
 
-// Создаем корневой элемент для рендеринга
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// Рендерим приложение
 root.render(
   <React.StrictMode>
     <App />
